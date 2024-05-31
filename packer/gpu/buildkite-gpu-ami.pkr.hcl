96b01fd314e7ba10734212d7cc20a9b85c3ef688
kevin <kevin@anyscale.com>
2024-05-31 09:21:05 +0000

    p
    Signed-off-by: kevin <kevin@anyscale.com>

--- packer/gpu/buildkite-gpu-ami.pkr.hcl
+++ packer/gpu/buildkite-gpu-ami.pkr.hcl
@@ -6,8 +6,8 @@ variable "region" {
 locals { timestamp = regex_replace(timestamp(), "[- TZ:]", "") }
 
 source "amazon-ebs" "gpu_box" {
-  ami_name        = "buildkite-stack-linux-gpu-${local.timestamp}"
-  ami_description = "Buildkite Elastic Stack (Amazon Linux 2 LTS w/ nvidia-docker)"
+  ami_name        = "vllm-buildkite-stack-linux-gpu-${local.timestamp}"
+  ami_description = "vLLM Buildkite Elastic Stack (Amazon Linux 2 LTS w/ nvidia-docker)"
   ami_groups      = ["all"]
   instance_type   = "g6.4xlarge"
   launch_block_device_mappings {
@@ -20,11 +20,14 @@ source "amazon-ebs" "gpu_box" {
   source_ami_filter {
     filters = {
       architecture = "x86_64"
-      # Sync this with the default buildkite elastic stack AMI.
-      name         = "buildkite-stack-linux-x86_64-2024-05-27T04-51-04Z-us-west-2"
+      # Sync this with the default buildkite elastic stack AMI
+      # https://s3.amazonaws.com/buildkite-aws-stack/v6.21.0/aws-stack.yml
+      # Mappings: AWSRegion2AMI: us-west-2: linuxamd64
+      # The name can be retrieved by querying the AMI on AWS
+      name         = "buildkite-stack-linux-x86_64-2024-05-27T04-51-04Z-us-west-2" 
     }
     most_recent = true
-    owners      = ["172840064832"]
+    owners      = ["172840064832"] # Buildkite AWS account ID
   }
   ssh_username = "ec2-user"
 }
--- packer/gpu/scripts/install-nvidia-docker.sh
+++ packer/gpu/scripts/install-nvidia-docker.sh
@@ -6,17 +6,19 @@ sudo yum install -y kernel-devel-$(uname -r) kernel-headers-$(uname -r) dkms
 
 sudo yum install -y pkgconfig libglvnd-devel
 
-DRIVER_CUDA_120="https://us.download.nvidia.com/tesla/525.147.05/NVIDIA-Linux-x86_64-525.147.05.run"
+readonly NVIDIA_DRIVER_VERSION="525.147.05" # Driver version for CUDA 12.0, Linux 64-bit, and L4 GPU
+NVIDIA_DRIVER="https://us.download.nvidia.com/tesla/${NVIDIA_DRIVER_VERSION}/NVIDIA-Linux-x86_64-${NVIDIA_DRIVER_VERSION}.run"
 
-curl -Lsf -o cuda_toolkit.run "${DRIVER_CUDA_120}"
-sudo dnf install -y kernel-modules-extra
-sudo sh cuda_toolkit.run --silent --dkms
+curl -Lsf -o cuda_toolkit.run "${NVIDIA_DRIVER}" # Load runfile into cuda_toolkit.run
+sudo dnf install -y kernel-modules-extra # https://github.com/amazonlinux/amazon-linux-2023/issues/538
+sudo sh cuda_toolkit.run --silent --dkms # Install the driver
 rm cuda_toolkit.run
 
-# nvidia container toolkit
-curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo | sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
+# Install nvidia container toolkit
+curl -sfL https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo | sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
 sudo yum install -y nvidia-container-toolkit nvidia-container-runtime
 
+# Update Docker configuration
 sudo nvidia-ctk runtime configure --runtime=docker
 sudo systemctl restart docker
 

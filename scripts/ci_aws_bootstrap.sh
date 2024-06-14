707208e7aef55f5923d4ad540c5c296a722c3f65
kevin <kevin@anyscale.com>
2024-06-14 06:02:42 +0000

    p
    Signed-off-by: kevin <kevin@anyscale.com>

--- scripts/ci_aws_bootstrap.sh
+++ scripts/ci_aws_bootstrap.sh
@@ -45,6 +45,8 @@ if [[ TRIGGER -eq 1 ]]; then
     exit 0
 fi
 
+echo "Uploading pipeline..."
+
 ls .buildkite || buildkite-agent annotate --style error 'Please merge upstream main branch for buildkite CI'
 curl -sSfL https://github.com/mitsuhiko/minijinja/releases/latest/download/minijinja-cli-installer.sh | sh
 source /var/lib/buildkite-agent/.cargo/env

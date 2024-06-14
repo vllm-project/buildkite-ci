c7178b86936c524a5888c112ebad7e1d07b2736c
Kevin H. Luu <kevin@anyscale.com>
2024-06-14 11:06:28 -0700

    Update ci_aws_bootstrap.sh

--- scripts/ci_aws_bootstrap.sh
+++ scripts/ci_aws_bootstrap.sh
@@ -2,57 +2,52 @@
 
 set -euo pipefail
 
-TRIGGER=0
-
-if [[ "${BUILDKITE_BRANCH}" == "main" ]]; then
-    echo "Run full tests on main"
-    TRIGGER=1
-fi
-
-get_diff() {
-    echo $(git diff --name-only --diff-filter=ACM $(git merge-base origin/main HEAD))
-}
-
-if [[ $TRIGGER -ne 1 ]]; then
-    diff=$(get_diff)
-
-    patterns=(
-        ".buildkite/"
-        ".github/"
-        "cmake/"
-        "benchmarks/"
-        "csrc/"
-        "tests/"
-        "vllm/"
-        "Dockerfile"
-        "format.sh"
-        "pyproject.toml"
-        "requirements*"
-        "setup.py"
-    )
-    for file in $diff; do
-        for pattern in "${patterns[@]}"; do
-            if [[ $file == $pattern* ]] || [[ $file == $pattern ]]; then
-                TRIGGER=1
-                echo "Found relevant changes: $file"
-                break
-            fi
-        done
-    done
-fi
-
-if [[ $TRIGGER -ne 1 ]]; then
-    echo "No relevant changes found to trigger tests."
-    exit 0
-fi
-
-if [[ $TRIGGER -eq 1 ]]; then
+upload_pipeline() {
     echo "Uploading pipeline..."
-
     ls .buildkite || buildkite-agent annotate --style error 'Please merge upstream main branch for buildkite CI'
     curl -sSfL https://github.com/mitsuhiko/minijinja/releases/latest/download/minijinja-cli-installer.sh | sh
     source /var/lib/buildkite-agent/.cargo/env
     cd .buildkite && minijinja-cli test-template-aws.j2 test-pipeline.yaml > pipeline.yml
     cat pipeline.yml
     buildkite-agent pipeline upload pipeline.yml
+    exit 0
+}
+
+get_diff() {
+    echo $(git diff --name-only --diff-filter=ACM $(git merge-base origin/main HEAD))
+}
+
+if [[ "${BUILDKITE_BRANCH}" == "main" ]]; then
+    echo "Run full tests on main"
+    upload_pipeline
 fi
+
+
+diff=$(get_diff)
+
+patterns=(
+    ".buildkite/"
+    ".github/"
+    "cmake/"
+    "benchmarks/"
+    "csrc/"
+    "tests/"
+    "vllm/"
+    "Dockerfile"
+    "format.sh"
+    "pyproject.toml"
+    "requirements*"
+    "setup.py"
+)
+for file in $diff; do
+    for pattern in "${patterns[@]}"; do
+        if [[ $file == $pattern* ]] || [[ $file == $pattern ]]; then
+            TRIGGER=1
+            echo "Found relevant changes: $file"
+            upload_pipeline
+        fi
+    done
+done
+
+echo "No relevant changes found to trigger tests."
+exit 0

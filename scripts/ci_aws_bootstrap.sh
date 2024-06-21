27321e098abb23e39d5d2917ca5b67d4171f5d1b
Kevin H. Luu <kevin@anyscale.com>
2024-06-21 15:59:07 -0700

    Update ci_aws_bootstrap.sh

--- scripts/ci_aws_bootstrap.sh
+++ scripts/ci_aws_bootstrap.sh
@@ -7,8 +7,8 @@ upload_pipeline() {
     ls .buildkite || buildkite-agent annotate --style error 'Please merge upstream main branch for buildkite CI'
     curl -sSfL https://github.com/mitsuhiko/minijinja/releases/latest/download/minijinja-cli-installer.sh | sh
     source /var/lib/buildkite-agent/.cargo/env
-    curl -o test-template.j2 https://raw.githubusercontent.com/vllm-project/buildkite-ci/main/scripts/test-template-aws.j2
-    cd .buildkite && minijinja-cli test-template-aws.j2 test-pipeline.yaml > pipeline.yml
+    curl -o .buildkite/test-template.j2 https://raw.githubusercontent.com/vllm-project/buildkite-ci/main/scripts/test-template-aws.j2
+    cd .buildkite && minijinja-cli test-template.j2 test-pipeline.yaml > pipeline.yml
     cat pipeline.yml
     buildkite-agent pipeline upload pipeline.yml
     exit 0

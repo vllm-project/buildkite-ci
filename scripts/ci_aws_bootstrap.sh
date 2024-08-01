276b81f161b2ea7fdfc3aa3d920e2f8da7e64567
Kevin H. Luu <kevin@anyscale.com>
2024-08-01 02:37:15 -0700

    bootstrap with list of diff files

--- scripts/ci_aws_bootstrap.sh
+++ scripts/ci_aws_bootstrap.sh
@@ -17,55 +17,17 @@ upload_pipeline() {
     if [ ! -e ".buildkite/test-template.j2" ]; then
         curl -o .buildkite/test-template.j2 https://raw.githubusercontent.com/vllm-project/buildkite-ci/main/scripts/test-template-aws.j2
     fi
-    cd .buildkite && minijinja-cli test-template.j2 test-pipeline.yaml > pipeline.yml
-    cat pipeline.yml
+    cd .buildkite
+    echo $list_file_diff
+    minijinja-cli test-template.j2 test-pipeline.yaml -D list_file_diff="$list_file_diff" > pipeline.yml
     buildkite-agent pipeline upload pipeline.yml
     exit 0
 }
 
 get_diff() {
     $(git add .)
-    echo $(git diff --name-status --diff-filter=ACMDR $(git merge-base origin/main HEAD))
+    echo $(git diff --name-only --diff-filter=ACMDR $(git merge-base origin/main HEAD))
 }
 
-if [[ "${RUN_ALL:-}" == "1" ]]; then
-    upload_pipeline
-fi
-
-if [[ "${BUILDKITE_BRANCH}" == "main" ]]; then
-    echo "Run full tests on main"
-    upload_pipeline
-fi
-
-
-diff=$(get_diff)
-
-patterns=(
-    ".buildkite/"
-    ".github/"
-    "cmake/"
-    "benchmarks/"
-    "csrc/"
-    "tests/"
-    "vllm/"
-    "Dockerfile"
-    "format.sh"
-    "pyproject.toml"
-    "CMakeLists.txt"
-    "requirements*"
-    "setup.py"
-    "test_docs/"
-    "docs/"
-)
-for file in $diff; do
-    for pattern in "${patterns[@]}"; do
-        if [[ $file == $pattern* ]] || [[ $file == $pattern ]]; then
-            TRIGGER=1
-            echo "Found relevant changes: $file"
-            upload_pipeline
-        fi
-    done
-done
-
-echo "No relevant changes found to trigger tests."
-exit 0
+list_file_diff=$(get_diff | tr ' ' '|')
+upload_pipeline

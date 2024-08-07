c9c287ee26d009b5a5bc5f1d33fb677d4a973706
kevin <kevin@anyscale.com>
2024-08-07 21:54:42 +0000

    add tpu nodes
    Signed-off-by: kevin <kevin@anyscale.com>

---
+++ infra/gcp/.terraform.lock.hcl
@@ -0,0 +1,21 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/google-beta" {
+  version = "5.40.0"
+  hashes = [
+    "h1:oZ9ZLX23Y4NvXkqQWpk8hG2YlY4eYxDokbDDjTV9O8M=",
+    "zh:00ea69f59764ad54fc5c97a4b46c649fbd77181de3843277b5fe2dc03846a46d",
+    "zh:1a9d2320d17a9cfa10ce3153be6121d5a8ea9676c42f60153341f42e99be04ae",
+    "zh:1c293175cff1d5316f3f56e7517ebc6fb1e7d45fbc7c844371f8e0640671a35f",
+    "zh:1cb294f4f805a220e7177e28800788c5afb4520ccd2d5204f5602dc6c81ce309",
+    "zh:3f352c88f5c8b2ffe2e301ce0a0c381559a63f8b6c8c17f189dc4a42ae0ed7a5",
+    "zh:48b72165d5c5edf0e3a5c27e76c132789760e5256e78e5cf2ca1c968791c06e2",
+    "zh:6274b561d1697bc9901d218ef22e4b9202f4fe7be688cd895c744151f6e416e0",
+    "zh:71ae311789879248a0ed5daa22971c34111ec93a7e6a44c3905fbabcd37d6c3a",
+    "zh:a0b695d4b209638953a3692c882f15917ef0329aa454410314ff6e21c5ca41dd",
+    "zh:a3824dfb6df374f1009bd7a24f5acde5b2c2780362fbc3334bc68b132d2a8525",
+    "zh:ac200d49c72ba4ec5b8492d816eb564ea57a28448a87ac567c6b9f4c81465d5a",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
---
+++ infra/gcp/main.tf
@@ -0,0 +1,110 @@
+provider "google-beta" {
+  project = "vllm-405802"
+  region  = "us-south1-a"
+}
+
+variable "project_id" {
+  default = "vllm-405802"
+}
+
+variable "buildkite_agent_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "huggingface_token" {
+  type      = string
+  sensitive = true
+}
+
+data "google_tpu_v2_runtime_versions" "available" {
+  provider = google-beta
+  zone = "us-south1-a"
+}
+
+resource "google_compute_disk" "disk" {
+  provider = google-beta
+  count = 8
+
+  name  = "tpu-disk-${count.index}"
+  size  = 512
+  type  = "pd-ssd"
+  zone  = "us-south1-a"
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  provider = google-beta
+
+  name          = "tpu-subnet"
+  ip_cidr_range = "10.0.0.0/16"
+  region        = "us-south1"
+  network       = google_compute_network.network.id
+}
+
+resource "google_compute_network" "network" {
+  provider = google-beta
+
+  name                    = "tpu-net"
+  auto_create_subnetworks = false
+}
+
+resource "google_tpu_v2_vm" "tpu" {
+  provider = google-beta
+  count = 8
+  name = "vllm-tpu-${count.index}"
+  zone = "us-south1-a"
+
+  runtime_version = "v2-alpha-tpuv5-lite"
+
+  accelerator_type = "v5litepod-1"
+
+  data_disks {
+    source_disk = google_compute_disk.disk[count.index].id
+    mode = "READ_WRITE"
+  }
+
+  network_config {
+    network = "default"
+    enable_external_ips = true
+  }
+
+  metadata = {
+    "startup-script" = <<-EOF
+      #!/bin/bash
+
+      apt-get update
+      apt-get install -y curl build-essential jq
+
+      curl -o- https://get.docker.com/ | bash -
+
+      curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+      /root/.cargo/bin/cargo install minijinja-cli
+      cp /root/.cargo/bin/minijinja-cli /usr/bin/minijinja-cli
+      chmod 777 /usr/bin/minijinja-cli
+
+      curl -fsSL https://keys.openpgp.org/vks/v1/by-fingerprint/32A37959C2FA5C3C99EFBC32A79206696452D198 | sudo gpg --dearmor -o /usr/share/keyrings/buildkite-agent-archive-keyring.gpg
+      echo "deb [signed-by=/usr/share/keyrings/buildkite-agent-archive-keyring.gpg] https://apt.buildkite.com/buildkite-agent stable main" | sudo tee /etc/apt/sources.list.d/buildkite-agent.list
+      apt-get update
+      apt-get install -y buildkite-agent
+
+      sudo usermod -a -G docker buildkite-agent
+      sudo -u buildkite-agent gcloud auth configure-docker us-central1-docker.pkg.dev --quiet
+
+      sudo sed -i "s/xxx/${var.buildkite_agent_token}/g" /etc/buildkite-agent/buildkite-agent.cfg
+      echo 'tags="queue=tpu"' | sudo tee -a /etc/buildkite-agent/buildkite-agent.cfg
+      echo 'HF_TOKEN=${var.huggingface_token}' | sudo tee -a /etc/environment
+
+      sudo mkfs.ext4 -m 0 -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb
+      sudo mkdir -p /mnt/disks/persist
+      sudo mount -o discard,defaults /dev/sdb /mnt/disks/persist
+
+      jq ". + {\"data-root\": \"/mnt/disks/persist\"}" /etc/docker/daemon.json > /tmp/daemon.json.tmp && mv /tmp/daemon.json.tmp /etc/docker/daemon.json
+      systemctl stop docker
+      systemctl daemon-reload
+      systemctl start docker
+
+      systemctl enable buildkite-agent
+      systemctl start buildkite-agent
+    EOF
+  }
+}

ce905e539c0f3b8dfad33f6060cbedf04a2d541c
kevin <kevin@anyscale.com>
2024-10-07 23:18:27 +0000

    p
    Signed-off-by: kevin <kevin@anyscale.com>

--- scripts/pipeline_generator/pipeline_generator.py
+++ scripts/pipeline_generator/pipeline_generator.py
@@ -5,28 +5,42 @@ from typing import List
 from pydantic import BaseModel, field_validator
 
 
-class PipelineGeneratorConfig(BaseModel):
-    run_all: bool
-    list_file_diff: List[str]
-    container_registry: str
-    container_registry_repo: str
-    commit: str
-    test_path: str  # List of tests
-    external_hardware_test_path: str  # List of external hardware tests
-    pipeline_file_path: str  # Path to the output pipeline file
-
-    @property
-    def container_image(self) -> str:
-        return f"{self.container_registry}/{self.container_registry_repo}:{self.commit}"
-
-    @field_validator("commit")
-    @classmethod
-    def check_commit(cls, value: str) -> str:
+class PipelineGeneratorConfig:
+    def __init__(
+        self,
+        container_registry: str,
+        container_registry_repo: str,
+        commit: str,
+        test_path: str,  # List of tests
+        external_hardware_test_path: str,  # List of external hardware tests
+        pipeline_file_path: str,  # Path to the output pipeline file
+        run_all: bool = False,
+        list_file_diff: List[str] = []
+    ):
+        self.run_all = run_all
+        self.list_file_diff = list_file_diff
+        self.container_registry = container_registry
+        self.container_registry_repo = container_registry_repo
+        self.commit = commit
+        self.test_path = test_path
+        self.external_hardware_test_path = external_hardware_test_path
+        self.pipeline_file_path = pipeline_file_path
+        self.container_image = f"{self.container_registry}/{self.container_registry_repo}:{self.commit}"
+    
+    def validate(self):
+        """Validate the configuration."""
         # Check if commit is a valid Git commit hash
         pattern = r"^[0-9a-f]{40}$"
-        if not re.match(pattern, value):
-            raise ValueError(f"Commit {value} is not a valid Git commit hash")
-        return value
+        if not re.match(pattern, self.commit):
+            raise ValueError(f"Commit {self.commit} is not a valid Git commit hash")
+
+        # Check if test_path exists
+        if not os.path.exists(self.test_path):
+            raise FileNotFoundError(f"Test file {self.test_path} not found")
+        
+        # Check if external_hardware_test_path exists
+        if not os.path.exists(self.external_hardware_test_path):
+            raise FileNotFoundError(f"External hardware test file {self.external_hardware_test_path} not found")
 
 
 class PipelineGenerator:
@@ -35,3 +49,4 @@ class PipelineGenerator:
             config: PipelineGeneratorConfig
         ):
         self.config = config
+        self.config.validate()

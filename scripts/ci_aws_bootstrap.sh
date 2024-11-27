741c7c1da92850a039954acb860a4394f0f4697e
Kevin H. Luu <kevin@anyscale.com>
2024-11-26 20:57:55 -0800

    Update ci_aws_bootstrap.sh

--- scripts/ci_aws_bootstrap.sh
+++ scripts/ci_aws_bootstrap.sh
@@ -38,6 +38,7 @@ upload_pipeline() {
     echo "Run all: $RUN_ALL"
     echo "Nightly: $NIGHTLY"
     minijinja-cli test-template.j2 test-pipeline.yaml -D branch="$BUILDKITE_BRANCH" -D list_file_diff="$LIST_FILE_DIFF" -D run_all="$RUN_ALL" -D nightly="$NIGHTLY" > pipeline.yml
+    cat pipeline.yml
     buildkite-agent pipeline upload pipeline.yml
     exit 0
 }
